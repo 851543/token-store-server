980c81a1101111ce6e535ecc19ee04a5b385592b
小气大方的阿俊 <130641570+851543@users.noreply.github.com>
2024-10-30 17:44:58 +0800

    更新商品部分代码

---
+++ token-server/src/main/java/com/token/controller/admin/GoodsController.java
@@ -0,0 +1,28 @@
+package com.token.controller.admin;
+
+import com.token.constant.MessageConstant;
+import com.token.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/goods")
+@Api(tags = "商品")
+@Slf4j
+public class GoodsController {
+
+    /**
+     * 新增商品
+     * @return
+     */
+    @PostMapping
+    @ApiOperation(value = "新增商品")
+    public Result<String> insert(){
+        return Result.success(MessageConstant.OPERATE_SUCCESS);
+    }
+}
