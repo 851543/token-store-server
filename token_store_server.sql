d19cf95019a343560da2be1b5d4b5953ab69b58e
小气大方的阿俊 <130641570+851543@users.noreply.github.com>
2024-11-06 19:50:12 +0800

    更新数据库

--- token_store_server.sql
+++ token_store_server.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80029
  File Encoding         : 65001
 
- Date: 28/10/2024 21:06:44
+ Date: 06/11/2024 19:48:44
 */
 
 SET NAMES utf8mb4;
@@ -20,8 +20,6 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 -- Table structure for address_book
 -- ----------------------------
-CREATE DATABASE token_store_server;
-USE token_store_server;
 DROP TABLE IF EXISTS `address_book`;
 CREATE TABLE `address_book`  (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
@@ -61,11 +59,14 @@ CREATE TABLE `category`  (
   `update_user` bigint NULL DEFAULT NULL COMMENT '修改人',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `idx_category_name`(`name`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin COMMENT = '商品及套餐分类' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 27 CHARACTER SET = utf8 COLLATE = utf8_bin COMMENT = '商品及套餐分类' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of category
 -- ----------------------------
+INSERT INTO `category` VALUES (23, 0, '新闻', 0, 0, '2024-11-01 16:37:24', '2024-11-01 16:37:24', 6, 6);
+INSERT INTO `category` VALUES (24, 0, '配送', 0, 0, '2024-11-01 17:15:35', '2024-11-01 17:15:35', 6, 6);
+INSERT INTO `category` VALUES (25, 0, '事实', 0, 0, '2024-11-01 17:15:44', '2024-11-01 17:15:44', 6, 6);
 
 -- ----------------------------
 -- Table structure for employee
@@ -86,14 +87,15 @@ CREATE TABLE `employee`  (
   `update_user` bigint NULL DEFAULT NULL COMMENT '修改人',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `idx_username`(`username`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8 COLLATE = utf8_bin COMMENT = '员工信息' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8 COLLATE = utf8_bin COMMENT = '员工信息' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of employee
 -- ----------------------------
 INSERT INTO `employee` VALUES (6, '超级用户', 'admin', 'e10adc3949ba59abbe56e057f20f883e', '13123556625', '男', '', 1, '2024-09-16 23:02:15', '2024-09-16 23:02:15', NULL, NULL);
 INSERT INTO `employee` VALUES (9, '猪bb', 'abcv', 'e10adc3949ba59abbe56e057f20f883e', '', '', '', 0, '2024-09-17 18:01:32', '2024-10-12 16:32:23', 0, 6);
-INSERT INTO `employee` VALUES (14, 'pwc', 'qqq', 'e10adc3949ba59abbe56e057f20f883e', '13123556625', '男', '', 0, '2024-10-12 20:49:30', '2024-10-12 20:49:30', 6, 6);
+INSERT INTO `employee` VALUES (14, 'pwc', 'qqq', '$2a$10$WiAmYaFywkNtMNgP1ke7Fuf/aNrmh/KCX9BZFLpz6k6Z.Cg6VO/wO', '13123556625', '男', '', 1, '2024-10-12 20:49:30', '2024-10-12 20:49:30', 6, 6);
+INSERT INTO `employee` VALUES (15, 'hh', 'hhh', 'e10adc3949ba59abbe56e057f20f883e', '13123556625', '男', '', 0, '2024-11-05 16:51:08', '2024-11-05 16:51:08', 14, 14);
 
 -- ----------------------------
 -- Table structure for goods
@@ -113,11 +115,17 @@ CREATE TABLE `goods`  (
   `update_user` bigint NULL DEFAULT NULL COMMENT '修改人',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `idx_dish_name`(`name`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin COMMENT = '商品' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 75 CHARACTER SET = utf8 COLLATE = utf8_bin COMMENT = '商品' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of goods
 -- ----------------------------
+INSERT INTO `goods` VALUES (70, '牛逼', 0, 0.00, '', '', 0, '2024-11-02 10:21:36', '2024-11-03 20:56:30', 6, 6);
+INSERT INTO `goods` VALUES (71, '梁雨烧烤', 23, NULL, '', '', 0, '2024-11-02 10:28:10', '2024-11-02 10:28:10', 6, 6);
+INSERT INTO `goods` VALUES (72, '蟒蛇', 23, 0.00, '', '', 0, '2024-11-02 10:28:48', '2024-11-02 10:28:48', 6, 6);
+INSERT INTO `goods` VALUES (73, '我也讨厌', 23, 0.00, '', '', 0, '2024-11-03 19:09:37', '2024-11-03 19:09:37', 6, 6);
+INSERT INTO `goods` VALUES (74, '哈哈', 23, 0.00, '', '', 0, '2024-11-03 19:10:26', '2024-11-03 19:10:26', 6, 6);
+INSERT INTO `goods` VALUES (75, '事实', 23, 0.00, '', '', 0, '2024-11-03 19:11:11', '2024-11-03 19:11:11', 6, 6);
 
 -- ----------------------------
 -- Table structure for goods_specs
@@ -129,11 +137,17 @@ CREATE TABLE `goods_specs`  (
   `name` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '规格名称',
   `value` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '规格数据list',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin COMMENT = '商品规格关系表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 116 CHARACTER SET = utf8 COLLATE = utf8_bin COMMENT = '商品规格关系表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of goods_specs
 -- ----------------------------
+INSERT INTO `goods_specs` VALUES (105, 71, '辣', '辣辣,微辣');
+INSERT INTO `goods_specs` VALUES (106, 72, '辣', '辣辣,微辣');
+INSERT INTO `goods_specs` VALUES (107, 73, '辣', '辣辣,微辣');
+INSERT INTO `goods_specs` VALUES (108, 74, '辣', '辣辣,微辣');
+INSERT INTO `goods_specs` VALUES (109, 75, '辣', '辣辣,微辣');
+INSERT INTO `goods_specs` VALUES (116, 70, '', '');
 
 -- ----------------------------
 -- Table structure for order_detail
@@ -255,6 +269,82 @@ CREATE TABLE `shopping_cart`  (
 
 -- ----------------------------
 -- Records of shopping_cart
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_emp_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_emp_role`;
+CREATE TABLE `sys_emp_role`  (
+  `emp_id` bigint NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `role_id` bigint NOT NULL DEFAULT 0 COMMENT '角色id',
+  PRIMARY KEY (`emp_id`, `role_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_emp_role
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_menu`;
+CREATE TABLE `sys_menu`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `menu_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'NULL' COMMENT '菜单名',
+  `path` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '路由地址',
+  `component` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '组件路径',
+  `visible` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '0' COMMENT '菜单状态（0显示 1隐藏）',
+  `status` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '0' COMMENT '菜单状态（0正常 1停用）',
+  `perms` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '权限标识',
+  `icon` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '#' COMMENT '菜单图标',
+  `create_by` bigint NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  `update_by` bigint NULL DEFAULT NULL,
+  `update_time` datetime NULL DEFAULT NULL,
+  `del_flag` int NULL DEFAULT 0 COMMENT '是否删除（0未删除 1已删除）',
+  `remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '菜单表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_menu
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `role_key` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '角色权限字符串',
+  `status` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '0' COMMENT '角色状态（0正常 1停用）',
+  `del_flag` int NULL DEFAULT 0 COMMENT 'del_flag',
+  `create_by` bigint NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  `update_by` bigint NULL DEFAULT NULL,
+  `update_time` datetime NULL DEFAULT NULL,
+  `remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '角色表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_role_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_menu`;
+CREATE TABLE `sys_role_menu`  (
+  `role_id` bigint NOT NULL AUTO_INCREMENT COMMENT '角色ID',
+  `menu_id` bigint NOT NULL DEFAULT 0 COMMENT '菜单id',
+  PRIMARY KEY (`role_id`, `menu_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_role_menu
 -- ----------------------------
 
 -- ----------------------------
