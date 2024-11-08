23f7f9baf00695fc07f800c2d7f76bda923728a2
小气大方的阿俊 <130641570+851543@users.noreply.github.com>
2024-11-08 10:55:26 +0800

    更新套餐模块代码

--- token-common/src/main/java/com/token/exception/CategoryIsExistException.java
+++ token-common/src/main/java/com/token/exception/CategoryIsExistException.java
@@ -1,6 +1,6 @@
 package com.token.exception;
 
-public class CategoryIsExistException extends RuntimeException {
+public class CategoryIsExistException extends BaseException {
     public CategoryIsExistException(String message) {
         super(message);
     }
